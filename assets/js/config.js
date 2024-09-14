3d695f6e97bd4527486b11e43f5db8ffd39b72cd
Jack - ThatDevWolfy <20j.alders@wildern.org>
2024-09-14 20:38:36 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,21 +1,4 @@
-const BADGES = [
-  {
-    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
-    image: "assets/badges/hypesquad_bravery.svg",
-  },
-  {
-    name: "Subscriber since Jun 29, 2022",
-    image: "assets/badges/nitro.svg",
-  },
-  {
-    name: "Server boosting Jun 29, 2022",
-    image: "assets/badges/boost.svg",
-  },
-  {
-    name: "That's an example! Change in config.js", // Message that appears when you hover over it
-    image: "assets/badges/bug_hunter_level2.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
-  },
-];
+const BADGES = [];
 
 const USER_ID = "1210341026808725605"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
 const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
